e6f8bfc64be35028a6c51381af61921fc70b1805
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-26 20:55:05 +0530

    fix(cask): update medha-keyboard cask version to 1.7.4 matching release URL

--- Casks/medha-keyboard.rb
+++ Casks/medha-keyboard.rb
@@ -1,8 +1,8 @@
 cask "medha-keyboard" do
-  version "1.7"
+  version "1.7.4"
   sha256 :no_check
 
-  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}.4/medhA-keyboard-macOS-v#{version}.dmg"
+  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}/medhA-keyboard-macOS-v1.7.dmg"
   name "medhA Keyboard Layout"
   desc "Sanskrit keyboard layout for macOS"
   homepage "https://www.lalitaalaalitah.com"
