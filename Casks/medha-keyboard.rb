b5171a45800a456444dc29a39245b007e5428da3
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-26 20:52:07 +0530

    chore: update download URL to point to v1.7.4 release assets

--- Casks/medha-keyboard.rb
+++ Casks/medha-keyboard.rb
@@ -2,7 +2,7 @@ cask "medha-keyboard" do
   version "1.7"
   sha256 :no_check
 
-  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}/medhA-keyboard-macOS-v#{version}.dmg"
+  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}.4/medhA-keyboard-macOS-v#{version}.dmg"
   name "medhA Keyboard Layout"
   desc "Sanskrit keyboard layout for macOS"
   homepage "https://www.lalitaalaalitah.com"
