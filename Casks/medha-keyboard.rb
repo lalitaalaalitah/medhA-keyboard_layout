b616397c4435de2b999f1868b26870bd9db21774
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-26 21:14:52 +0530

    release: v1.7.5 update medhA.bundle version and nix build path

--- Casks/medha-keyboard.rb
+++ Casks/medha-keyboard.rb
@@ -1,5 +1,5 @@
 cask "medha-keyboard" do
-  version "1.7.4"
+  version "1.7.5"
   sha256 :no_check
 
   url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}/medhA-keyboard-macOS-v1.7.dmg"
