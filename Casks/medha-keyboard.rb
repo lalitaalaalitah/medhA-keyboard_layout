088f4291b37997f23a30d644aea35c0b184b2085
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-26 20:19:11 +0530

    fix(cask): use dynamic version string in medha-keyboard cask URL

--- Casks/medha-keyboard.rb
+++ Casks/medha-keyboard.rb
@@ -2,7 +2,7 @@ cask "medha-keyboard" do
   version "1.7"
   sha256 :no_check
 
-  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}/medhA-keyboard-macOS-v1.7.dmg"
+  url "https://github.com/lalitaalaalitah/medhA-keyboard_layout/releases/download/v#{version}/medhA-keyboard-macOS-v#{version}.dmg"
   name "medhA Keyboard Layout"
   desc "Sanskrit keyboard layout for macOS"
   homepage "https://www.lalitaalaalitah.com"
